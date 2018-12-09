54eb5f30b92eabdc061d664ad034130a3a356ac8
Jose Garay <garayjrjose@gmail.com>
2018-12-08 19:32:44 -0800

    I think everything is not deleted anymore.

--- Equipment.cpp
+++
@@ -1,9 +0,0 @@
-/**************************************************************************************************
-** Author: Jose Garay
-** Date: 12/01/2018
-** Description: The Implementation File for the Equipment Class. Abstract class that carries all the 
-basic attributes of the Equipment that will be used in the game. Inherits from the Item class.
-**************************************************************************************************/
-#include "Equipment.hpp"	
-Equipment::~Equipment(){}
-Equipment::Equipment(){}
